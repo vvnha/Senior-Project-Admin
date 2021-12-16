159e4588295f00f172572c3932599b0b7284ab35
NhaVoVan <38065969+vvnha@users.noreply.github.com>
2021-12-16 15:08:51 +0700

    master -feat- admin

---
+++ routes/fields.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-mongoose');
+const { fields } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('fields');
+
+// This file contains the logic of every route in Forest Admin for the collection fields:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Field
+router.post('/fields', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Field
+router.put('/fields/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Field
+router.delete('/fields/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Fields
+router.get('/fields', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Fields
+router.get('/fields/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Field
+router.get('/fields/\\b(?!count\\b):recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Fields
+router.get('/fields.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Fields
+router.delete('/fields', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
---
+++ routes/materials.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-mongoose');
+const { materials } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('materials');
+
+// This file contains the logic of every route in Forest Admin for the collection materials:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Material
+router.post('/materials', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Material
+router.put('/materials/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Material
+router.delete('/materials/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Materials
+router.get('/materials', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Materials
+router.get('/materials/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Material
+router.get('/materials/\\b(?!count\\b):recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Materials
+router.get('/materials.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Materials
+router.delete('/materials', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
